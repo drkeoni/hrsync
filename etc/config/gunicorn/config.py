339b3798948695dcfb10d725425e7327dc387d05
drkeoni <jon_m_sorenson@yahoo.com>
2018-09-02 18:08:33 -0700

    moving to Docker image model for deploy

---
+++ etc/config/gunicorn/config.py
@@ -0,0 +1,11 @@
+# gunicorn config file
+
+pidfile = '/tmp/gunicorn_hrsync.pid'
+workers = 1
+bind = '0.0.0.0:8421'
+accesslog = '/opt/log/hrsync/gunicorn-access.log'
+errorlog = '/opt/log/hrsync/gunicorn-error.log'
+loglevel = 'info'
+timeout = 1800
+graceful_timeout = 1800
+keepalive = 600
