1b3f7021366c4afac3c8470a55456e2d8094b7f7
drkeoni <jon_m_sorenson@yahoo.com>
2018-10-25 21:52:51 -0700

    supporting download for an arbitrary date range

--- bin/fetch_hr_data.py
+++ bin/fetch_hr_data.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import logging
 import json
+import datetime as dt
 
 from fitbit import Fitbit
 from oauthlib.oauth2.rfc6749.errors import TokenExpiredError
@@ -16,15 +17,19 @@ ch = logging.StreamHandler()
 ch.setFormatter(logging.Formatter(LOG_FORMAT))
 log.addHandler(ch)
 
-ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..'))
-AUTH_FILE = os.path.join(ROOT,'.fitbit_auth.json')
-
+ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+AUTH_FILE = os.path.join(ROOT, '.fitbit_auth.json')
+DATE_FORMAT = '%Y-%m-%d'
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--period',default='1d',
+    parser.add_argument('--period', default='1d',
                         help='Period of time for heart-rate data.  '
                              'Examples: 1d, 1w, 30d, 6m'
+                       )
+    parser.add_argument('--start_date', default=None,
+                        help='If specified use this date as the start date '
+                             'for querying the API (until now).  Format yyyy-mm-dd'
                        )
     args = parser.parse_args()
     return args
@@ -51,7 +56,16 @@ def run(args):
             expires_at=auth_data['expires_at'])
 
     try:
-        hr_data = fitbit_api.time_series('heart', period=args.period)
+        if args.start_date:
+            try:
+                start_date = dt.datetime.strptime(args.start_date, DATE_FORMAT)
+            except ValueError:
+                log.error("'{}' does not look like a date in format {}".format(args.start_date, DATE_FORMAT))
+                return 1
+            end_date = dt.datetime.now()
+            hr_data = fitbit_api.time_series('heart', base_date=start_date, end_date=end_date)
+        else:
+            hr_data = fitbit_api.time_series('heart', period=args.period)
     except TokenExpiredError as e:
         log.exception(e)
         log.error('You need to reauthenticate with fitbit oauth')
@@ -64,6 +78,7 @@ def run(args):
     print(json.dumps(hr_data, indent=4))
 
     return 0
+
 
 if __name__ == '__main__':
     args = parse_args()
